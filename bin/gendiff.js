e15ac460d306ffc51989940e610d965444b2d3e6
Maria Berina <gia0sam8cint@bk.ru>
2024-04-26 14:42:07 +0300

    Change bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const program = new Command();
 program
   .version('12.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format', 'json')
+  .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, format) => {
     console.log(genDiff(filepath1, filepath2, format));
