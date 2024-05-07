4ee6dc0c3f079b648b7680b3f206e677d404add5
Maria Berina <gia0sam8cint@bk.ru>
2024-05-07 12:30:25 +0300

    Create bin/file1.json, bin/file2.json, change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { cwd } from 'node:process';
+import { path } from 'node:path';
+
 const program = new Command();
 
 program
@@ -9,7 +12,13 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, format) => {
-    console.log(genDiff(filepath1, filepath2, format));
+    // console.log(genDiff(filepath1, filepath2, format));
+    const dir1 = cwd(filepath1);
+    const dir2 = cwd(filepath2);
+    console.log(dir2);
+    console.log(dir1);
+    console.log(path.resolve(dir1, filepath1));
+    //path.resolve('/dir2','/filepath2');
   });
 
 program.parse();
