3b273b0a277b577c469c2d218bfd585cc10ee0c1
Maria Berina <gia0sam8cint@bk.ru>
2024-05-08 17:05:16 +0300

    Change bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,8 @@
 
 import { Command } from 'commander';
 import { cwd } from 'node:process';
-import { path } from 'node:path';
+import path from 'path';
+import fs from 'fs';
 
 const program = new Command();
 
@@ -15,11 +16,20 @@ program
     // console.log(genDiff(filepath1, filepath2, format));
     const dir1 = cwd(filepath1);
     const dir2 = cwd(filepath2);
-    console.log(dir2);
-    console.log(dir1);
-    console.log(path.resolve(dir1, filepath1));
-    //path.resolve('/dir2','/filepath2');
+    // console.log(path.resolve(dir1, filepath1));
+    // console.log(path.resolve(dir2,filepath2));
+    const file1 = path.resolve(dir1, filepath1);
+    const file2 = path.resolve(dir2,filepath2);
+    // return [file1, file2];
   });
 
-program.parse();
+program
+  .action(() => {
+    const read1 = fs.readFileSync(file1);
+    const read2 = fs.readFileSync(file2);
+    const json1 = JSON.parse(read1);
+    const json2 = JSON.parse(read2);
+    console.log(json1);
+    console.log(json2);
+  });
 
