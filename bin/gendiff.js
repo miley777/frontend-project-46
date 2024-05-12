4bcdbcb9d34796983aa352a4f6c1ba6c3260b6ee
Maria Berina <gia0sam8cint@bk.ru>
2024-05-13 01:07:55 +0300

    Fix src/index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import genDiff from '../src/index.js'
+
 
 const program = new Command();
-
+import genDiff from '../src/index.js'
 program
   .version('12.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, format) => {
-    console.log(genDiff(filepath1, filepath2));
+    genDiff(filepath1, filepath2);
   });
 
+program.parse();
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { cwd } from 'node:process';
+import process from 'process';
 import path from 'path';
 import fs from 'fs';
 import getParsed from './parsing.js'
 
-const getFilePath = (file) => path.resolve(cwd(), file);
+const getFilePath = (file) => path.resolve(process.cwd(), file);
 
 const getData = (file) => fs.readFileSync(getFilePath(file), 'utf-8');
 
 const genDiff = (file1, file2) => {
-  const data1 = getData(file);
+  const data1 = getData(file1);
   const data2 = getData(file2);
   const parsing1 = getParsed(file1, data1);
   const parsing2 = getParsed(file2, data2);
