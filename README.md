4d51616767b9d334bc9be246faf09694df47d050
Maria Berina <gia0sam8cint@bk.ru>
2024-06-13 15:54:23 +0300

    Change src/compare.js, README.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/miley777/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/miley777/frontend-project-46/actions)
+
+https://asciinema.org/a/5nIgrvD83nQenFbpzTXZmKPR2
+
--- src/compare.js
+++ src/compare.js
@@ -7,44 +7,21 @@ const getComparedFiles = (filepath1, filepath2) => {
   const uniqKeys = _.uniq([ ...keys1, ...keys2 ]);
   const sortedKeys = _.sortBy(uniqKeys);
   const result = {};
+
   
-  //const mapKey = sortedKeys.map((key) => {
-    //if (!Object.hasOwn(filepath1, key)) {
-      //return `+  ${key}: ${filepath2[key]}`;
-    //} else if (!Object.hasOwn(filepath2, key)) {
-      //return `-  ${key}: ${filepath1[key]}`;
-    //} else if (filepath1[key] !== filepath2[key]) {
-      //return `-  ${key}: ${filepath1[key]}\n  + ${key}: ${filepath2[key]}`;
-    //} else if (filepath1[key] === filepath2[key]) {
-     // return `   ${key}: ${filepath2[key]}`;
-   // }
- // });
-
-  //for (const key of sortedKeys) {
-    //if (!Object.hasOwn(filepath1, key)) {
-      //result[key] = 'added';
-    //} else if (!Object.hasOwn(filepath2, key)) {
-      //result[key] = 'deleted';
-    //} else if (filepath1[key] !== filepath2[key]) {
-      //result[key] = 'changed';
-    //} else if (filepath1[key] === filepath2[key]) {
-      //result[key] = 'unchanged';
-    //}
-
-    for (const key of sortedKeys) {
+  const mapKey = sortedKeys.map((key) => {
     if (!Object.hasOwn(filepath1, key)) {
-      result['+ ' + key] = filepath2[key];
+      return `  + ${key}: ${filepath2[key]}`;
     } else if (!Object.hasOwn(filepath2, key)) {
-      result['- ' + key] = filepath1[key];
+      return `  - ${key}: ${filepath1[key]}`;
     } else if (filepath1[key] !== filepath2[key]) {
-      result['- ' + key] = filepath1[key];
-      result['+ ' + key] = filepath2[key];
+      return `  - ${key}: ${filepath1[key]}\n  + ${key}: ${filepath2[key]}`;
     } else if (filepath1[key] === filepath2[key]) {
-      result['  ' + key] = filepath2[key];
+      return `    ${key}: ${filepath2[key]}`;
     }
-  };
+  });
 
-  return result;
+  return [ '{', mapKey.join('\n') , '}'].join('\n');
 };
 
 export default getComparedFiles;
