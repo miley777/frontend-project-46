3e336d72a510d532a8cbd40f8c976e7f67b5b315
Maria Berina <gia0sam8cint@bk.ru>
2024-06-28 13:58:35 +0300

    Change Makefile, __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@
 //import { getData } from '../src';
 import fs from 'fs';
 import getFixturePath from './helper';
-//import   genDiff  from '../src';
+import   genDiff  from '../src';
 import { expect, test} from '@jest/globals';
 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
@@ -11,7 +11,7 @@ test.each([ {file1: './__tests__/../__fixtures__/file1.json', file2: './__tests_
   {file1: './__fixtures__/file1.json', file2: './__fixtures__/file2.json'},
   {file1: './bin/../__fixtures__/file1.json', file2: './bin/../__fixtures__/file2.json'},
   {file1: './src/../__fixtures__/file1.json', file2: './src/../__fixtures__/file2.json'},
-]), ('gendiff', () => {
+]) ('gendiff', () => {
   expect(genDiff(file1, file2)).toEqual(readFile(__fixtures__/confirmingFile1.txt));
 });
 
