b96080813041495893f08345e16ff0b5b1adfff2
Maria Berina <gia0sam8cint@bk.ru>
2024-08-01 14:33:09 +0300

    Update src/compare.js, create src/diff.js

--- src/compare.js
+++ src/compare.js
@@ -1,8 +1,47 @@
-import _ from 'lodash';
+//import _ from 'lodash';
+import { getType } from './diff.js';
 
 
-const getComparedFiles = (filepath1, filepath2) => {
-  //const keys1 = _.keys(filepath1);
+
+const iter = (typedKeys, spaceCount = 4, depth = 1, replacer = ' ') => {
+  const indentSize = depth * spaceCount;
+  const shiftToTheLeft = spaceCount - 2;
+  const currentIndent = replacer.repeat(indentSize - shiftToTheLeft);
+  const bracketIndent = replacer.repeat(indentSize - spaceCount);
+  const sortedEntries = typedKeys.map((typedKey) => {
+    if (typedKey.type === 'deleted') {
+      return `${currentIndent}- ${typedKey.key}: ${typedKey.value}`;
+    }
+    if (typedKey.type === 'added') {
+      return `${currentIndent}+ ${typedKey.key}: ${typedKey.value}`;
+    }
+    if (typedKey.type === 'unchanged') {
+      return `${currentIndent}  ${typedKey.key}: ${typedKey.value}`;
+    }
+    if (typedKey.type === 'changed') {
+      return `${currentIndent}- ${typedKey.key}: ${typedKey.value1}\n${currentIndent}+ ${typedKey.key}: ${typedKey.value2}`;
+    }
+    if (typedKey.type === 'nested') {
+      return `${currentIndent}  ${typedKey.key}: ${iter(typedKey.children, spaceCount, depth + 1, replacer)}`;
+    }
+  });
+  return [ '{', ...sortedEntries, `${bracketIndent}}`,].join('\n');
+}
+
+
+const getComparedLines = (filepath1, filepath2) => {
+  const typedKeys = getType(filepath1, filepath2);
+  const iterdiff = iter(typedKeys);
+  return iterdiff;
+};
+
+export default getComparedLines;
+
+
+
+
+
+//const keys1 = _.keys(filepath1);
   //const keys2 = _.keys(filepath2);
   //const uniqKeys = _.uniq([ ...keys1, ...keys2 ]);
   //const sortedKeys = _.sortBy(uniqKeys);
@@ -22,65 +61,10 @@ const getComparedFiles = (filepath1, filepath2) => {
 
   //return [ '{', mapKey.join('\n') , '}'].join('\n');
 
-  const changes = labeledKeys(filepath1, filepath2);
-  const entries1 = _.entries(filepath1);
-  const entries2 = _.entries(filepath2);
-  const allEntries = [ ...entries1, ...entries2 ];
-  const iters = iter(allEntries);
-  return iters;
-
-};
-
-const iter = (allEntries, spaceCount = 2, depth = 1, replacer = ' ') => {
-  const indentSize = depth * spaceCount;
-  const repeatedReplacer = replacer.repeat(indentSize);
-  const sortedEntries = allEntries.map(([key, val]) => {
-    if (!_.isObject(val)) {
-      return `${repeatedReplacer}${key}: ${val}`;
-    } else {
-      //depth += 1;
-      return `${repeatedReplacer}${key}: {/n${ iter(allEntries, spaceCount, depth + 1, replacer)}\n ${repeatedReplacer}}`
-    }
-  });
-  return sortedEntries.join('\n');
-}
-
-const labeledKeys = (filepath1, filepath2) => {
-  const keys1 = _.keys(filepath1);
-  const keys2 = _.keys(filepath2);
-  const uniqKeys = _.uniq([ ...keys1, ...keys2 ]);
-  const sortedKeys = _.sortBy(uniqKeys);
-  const result = {};
-  for (const key of sortedKeys) {
-    if (!Object.hasOwn(filepath1, key)) {
-      result[key] = 'added';
-    } else if (!Object.hasOwn(filepath2, key)) {
-      result[key] = 'deleted';
-    } else if (filepath1[key] !== filepath2[key]) {
-      result[key] = 'changed';
-    } else {
-      result[key] = 'unchanged';
-    }
-  }
-
-return result;
-};
-
-//const stringify = (value, replacer, spaceCount = 2, space = ' ') => {
-
-  //const iter = (value, depth) => {
-    //const indentSize = depth * spaceCount;
-    //const tab = space.repeat(indentSize);
-    //const values = Object.entries(value).map(([key, val]) => `${tab}${replacer} ${key}: ${iter(val, depth + 1)}`);
-
-    //return [ '{', ...values, `${tab}`, '}'].join('\n');
-  //}
-
-  //return iter(value, 1);
-//}
-
-//const repl = (value) => {
-
-//}
-
-export default getComparedFiles;
+  
+  //const entries1 = _.entries(filepath1);
+  //const entries2 = _.entries(filepath2);
+  //const allEntr = [ ...entries1, ...entries2 ];
+  //return ['{', iter(allEntries), '}'].join('\n');
+  //return JSON.stringify(promdiff(filepath1, filepath2));
+  //return promdiff(filepath1, filepath2);
