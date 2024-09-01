caf4a0a2acd9b0f946a18200f366aec2405f05d8
Maria Berina <gia0sam8cint@bk.ru>
2024-09-02 01:55:29 +0300

    Update __tests__/index.test.js, delete __tests__/helper.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,15 @@
 import { buildFilePath } from '../src';
 import { extractFormat } from '../src';
 import fs from 'fs';
-import getFixturePath from './helper';
 import   genDiff  from '../src';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { expect, test } from '@jest/globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
