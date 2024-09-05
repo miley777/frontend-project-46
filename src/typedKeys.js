0aa675211abb621a44a4c24400b27e238df555a5
Maria Berina <gia0sam8cint@bk.ru>
2024-09-05 16:40:52 +0300

    Update src/typedKeys.js

--- src/typedKeys.js
+++ src/typedKeys.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 
 const getType = (filepath1, filepath2) => {
-  //const keys1 = _.keys();
-  //const keys2 = _.keys();
-  //const uniqKeys = _.uniq([...keys1, ...keys2]);
-  const sortedKeys = _.sortBy(Object.keys({...filepath1, ...filepath2}));
+  // const keys1 = _.keys();
+  // const keys2 = _.keys();
+  // const uniqKeys = _.uniq([...keys1, ...keys2]);
+  const sortedKeys = _.sortBy(Object.keys({ ...filepath1, ...filepath2 }));
   const typedKeys = sortedKeys.map((key) => {
     const val1 = filepath1[key];
     const val2 = filepath2[key];
@@ -12,17 +12,15 @@ const getType = (filepath1, filepath2) => {
       return { type: 'added', key: key, value: val2 };
     }
     if (!Object.hasOwn(filepath2, key)) {
-      return { type: 'deleted', key: key, value: val1 };
+      return { type: 'deleted', key, value: val1 };
     }
     if (_.isObject(filepath1[key]) && _.isObject(filepath2[key])) {
-      return { type: 'nested', key: key, children: getType(val1, val2) };
+      return { type: 'nested', key, children: getType(val1, val2) };
     }
     if ((filepath1[key] !== filepath2[key])) {
-      return { type: 'changed', key: key, value1: val1, value2: val2 };
+      return { type: 'changed', key, value1: val1, value2: val2 };
     }
-    if (filepath1[key] === filepath2[key]) {
-      return { type: 'unchanged', key: key, value: val2 };
-    }
+    return { type: 'unchanged', key, value: val2 };
   });
   return typedKeys;
 };
