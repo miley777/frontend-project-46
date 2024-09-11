e689423cde2379af099c30b60785e703ad150d6c
Maria Berina <gia0sam8cint@bk.ru>
2024-09-12 02:26:18 +0300

    Update src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,38 +1,38 @@
 import _ from 'lodash';
 
 const getIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
-const bracketIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount);
+const closingBracketIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount);
 
-const stringify = (keys, spaceCount = 4, depth = 1) => {
+const stringify = (keys, depth = 1) => {
   if (!_.isObject(keys)) {
     return `${keys}`;
   }
   const objEntries = Object.entries(keys).map(
-    ([key, value]) => `  ${getIndent(depth, spaceCount)}${key}: ${stringify(value, spaceCount, depth + 1)}`,
+    ([key, value]) => `  ${getIndent(depth)}${key}: ${stringify(value, depth + 1)}`,
   );
-  return ['{', ...objEntries, `${bracketIndent(depth - 1, spaceCount)}}`].join('\n');
+  return ['{', ...objEntries, `${closingBracketIndent(depth - 1)}}`].join('\n');
 };
 
-const stylish = (typedKeys, spaceCount = 4, depth = 1) => {
+const stylish = (typedKeys, depth = 1) => {
   const sortedEntries = typedKeys.map((typedKey) => {
-    const elem1 = `${getIndent(depth)}- ${typedKey.key}: ${stringify(typedKey.value1, spaceCount, depth + 1)}`;
-    const elem2 = `${getIndent(depth)}+ ${typedKey.key}: ${stringify(typedKey.value2, spaceCount, depth + 1)}`;
+    const elem1 = `${getIndent(depth)}- ${typedKey.key}: ${stringify(typedKey.value1, depth + 1)}`;
+    const elem2 = `${getIndent(depth)}+ ${typedKey.key}: ${stringify(typedKey.value2, depth + 1)}`;
     switch (typedKey.type) {
       case 'deleted':
-        return `${getIndent(depth)}- ${typedKey.key}: ${stringify(typedKey.value, spaceCount, depth + 1)}`;
+        return `${getIndent(depth)}- ${typedKey.key}: ${stringify(typedKey.value, depth + 1)}`;
       case 'added':
-        return `${getIndent(depth)}+ ${typedKey.key}: ${stringify(typedKey.value, spaceCount, depth + 1)}`;
+        return `${getIndent(depth)}+ ${typedKey.key}: ${stringify(typedKey.value, depth + 1)}`;
       case 'unchanged':
         return `${getIndent(depth)}  ${typedKey.key}: ${typedKey.value}`;
       case 'changed':
         return [elem1, elem2].join('\n');
       case 'nested':
-        return `${getIndent(depth)}  ${typedKey.key}: ${stylish(typedKey.children, spaceCount, depth + 1)}`;
+        return `${getIndent(depth)}  ${typedKey.key}: ${stylish(typedKey.children, depth + 1)}`;
       default:
         return new Error(`Type: ${typedKey.key} is underfined`);
     }
   });
-  return ['{', ...sortedEntries, `${bracketIndent(depth - 1, spaceCount)}}`].join(
+  return ['{', ...sortedEntries, `${closingBracketIndent(depth - 1)}}`].join(
     '\n',
   );
 };
