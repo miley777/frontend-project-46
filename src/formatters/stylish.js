1d4ad033501d121afc9d604bc1774d3f1cc12073
Maria Berina <gia0sam8cint@bk.ru>
2024-09-12 23:33:37 +0300

    Update src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,40 +1,44 @@
 import _ from 'lodash';
 
 const getIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
-const closingBracketIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount);
+// const closingBracketIndent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount);
 
 const stringify = (keys, depth = 1) => {
   if (!_.isObject(keys)) {
     return `${keys}`;
   }
   const objEntries = Object.entries(keys).map(
-    ([key, value]) => `  ${getIndent(depth)}${key}: ${stringify(value, depth + 1)}`,
+    ([key, value]) => `  ${getIndent(depth + 1)}${key}: ${stringify(value, depth + 1)}`,
   );
-  return ['{', ...objEntries, `${closingBracketIndent(depth - 1)}}`].join('\n');
+  return ['{', ...objEntries, `  ${getIndent(depth)}}`].join('\n');
 };
 
-const stylish = (typedKeys, depth = 1) => {
-  const sortedEntries = typedKeys.map((typedKey) => {
-    const elem1 = `${getIndent(depth)}- ${typedKey.key}: ${stringify(typedKey.value1, depth + 1)}`;
-    const elem2 = `${getIndent(depth)}+ ${typedKey.key}: ${stringify(typedKey.value2, depth + 1)}`;
-    switch (typedKey.type) {
-      case 'deleted':
-        return `${getIndent(depth)}- ${typedKey.key}: ${stringify(typedKey.value, depth + 1)}`;
-      case 'added':
-        return `${getIndent(depth)}+ ${typedKey.key}: ${stringify(typedKey.value, depth + 1)}`;
-      case 'unchanged':
-        return `${getIndent(depth)}  ${typedKey.key}: ${typedKey.value}`;
-      case 'changed':
-        return [elem1, elem2].join('\n');
-      case 'nested':
-        return `${getIndent(depth)}  ${typedKey.key}: ${stylish(typedKey.children, depth + 1)}`;
-      default:
-        return new Error(`Type: ${typedKey.key} is underfined`);
-    }
-  });
-  return ['{', ...sortedEntries, `${closingBracketIndent(depth - 1)}}`].join(
-    '\n',
-  );
-};
+const stylish = (tree) => {
+  const iter = (node, depth) => {
+    const sortedEntries = node.map((typedKey) => {
+      switch (typedKey.type) {
+        case "deleted":
+          return `${getIndent(depth + 1)}- ${typedKey.key}: ${stringify(typedKey.value, depth + 1)}`;
+        case "added":
+          return `${getIndent(depth + 1)}+ ${typedKey.key}: ${stringify(typedKey.value, depth + 1)}`;
+        case "unchanged":
+          return `${getIndent(depth + 1)}  ${typedKey.key}: ${typedKey.value}`;
+        case "changed": {
+          const elem1 = `${getIndent(depth + 1)}- ${typedKey.key}: ${stringify(typedKey.value1, depth + 1)}`;
+          const elem2 = `${getIndent(depth + 1)}+ ${typedKey.key}: ${stringify(typedKey.value2, depth + 1)}`;
+          return [elem1, elem2].join("\n");
+        }
+        case "nested":
+          return `${getIndent(depth + 1)}  ${typedKey.key}: {\n${iter(typedKey.children, depth + 1).join("\n")}\n  ${getIndent(depth + 1)}}`;
+        default:
+          return new Error(`Type: ${typedKey.key} is underfined`);
+      }
+    });
+
+    return sortedEntries;
+  }
+ 
+  return ["{", ...iter(tree, 0), `}`].join("\n");
+}
 
 export default stylish;
